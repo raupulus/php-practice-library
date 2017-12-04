cf36ba2c7410bae12baf248e42d8768ebf5d9c9d
María Valderrama <valderramarodriguezm@gmail.com>
2017-12-04 15:49:29 +0100

    Updated autoloading.php

--- autoloading.php
+++ autoloading.php
@@ -9,8 +9,13 @@
  * → Una clase por fichero.
  * → El nombre del fichero es el nombre de la clase.
  *
+ * ATENCIÓN: Si las clases usan espacios de nombre no funciona bien, ya que la
+ * función traslada los espacios de nombre al sistema de ficheros.
+ * Sin embargo, si el espacio de nombre y la ruta del archivo coinciden sí
+ * funcionará si además le cambiamos la barra.
+ *
  * TODO: Comprobar si metiéndolo en una función continúa funcionando.
  */
 spl_autoload_register(function($c) {
-    include mb_strtolower($c) . '.php';
+    include mb_strtolower(str_replace('\\', '/', $c)) . '.php';
 });
